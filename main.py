2cf4cc0ba17d3f9690134f1ab93a5ae905750e99
MochaLatte <chraimire1@gmail.com>
2023-04-28 18:41:30 -0700

    Commented and cleaned up code

--- main.py
+++ main.py
@@ -19,15 +19,17 @@ from rich.console import Console
 from rich.table import Table
 from time import sleep
 
-console = Console()
-df = pd.DataFrame()
+console = Console()                 # Rich Funct. Displays to actual console
+df = pd.DataFrame()                 # Dataframe Funct from Pandas
 
+# Clear Current Console Output
 def clear_console():
     command = 'clear'
     if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
         command = 'cls'
     os.system(command)
 
+# Count Unique Values in DataFrame Column
 def count_unique_values(df, column):
     unique_values = {}
     for value in df[column]:
@@ -37,6 +39,7 @@ def count_unique_values(df, column):
             unique_values[value] = 1
     return unique_values
 
+# Count All Values in DataFrame Column
 def count_all_values(df, column):
     count = 0
     for value in df[column]:
@@ -44,8 +47,7 @@ def count_all_values(df, column):
     return count
 
 
-#!!!!!!!!!!!    Moises' "first few" function   
-
+# Moises' "first few" function   
 def first_5(df, quantity):
     first_5 = []
     first_5 = df.head(quantity).values.tolist()
@@ -55,6 +57,7 @@ def first_5(df, quantity):
 def current_time():
     return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
+# Display Main Menu
 def menuDisplay():
     print ("\
                     -----------------Menu--------------------- \n \
@@ -63,21 +66,32 @@ def menuDisplay():
                                 [c] Clear Console\n \
                                 [d] Done\n")
 
+# Load Dataset into DataFrame
 def load_data():
-    global df
+    
+    global df   # Apparently Necessary to allow other functs to access Dataframe
+
     print("\
                     -----------------Load Data Set--------------\n \
                     [1] data.csv                                 \n \
                     [2] N/A                                      \n \
                     [3] N/A                                      \n\n \
                     [d] Return To Main Menu          \n")
+    
+    # Get User's Selection
     file_choice = input(f"[{current_time()}] Select the number of the file to load from the list below: ")
+
+    # Switch for User's Selection
     if file_choice == "1":
+
         file_name = "data.csv"
+
         # Load data set and time how long it takes
         start_time = time.time()
+
         df = pd.read_csv(file_name)
         print('CSV file has been read into a pandas dataframe.')
+
         end_time = time.time()
 
         # Trimming Data Set
@@ -88,20 +102,25 @@ def load_data():
         df = df.rename(columns=lambda x: x.strip())                         #trim whitespace from headers
         df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)  #trim whitespace from cells
         
+        # Print Dataset Col/Row's Read
         print(f"[{current_time()}]" + " " + file_name)
         print(f"[{current_time()}] Total Columns Read: {df.shape[1]}")
         print(f"[{current_time()}] Total Rows Read: {df.shape[0]}\n")
         print("File loaded successfully!")
         print(f"Time to load {round(end_time - start_time, 2)} sec.\n")
+
     elif file_choice == "d":
-        clear_console()
+        clear_console()         # Clear Console Display
         return
     else:
         print("Invalid choice. Returning to the main menu.")
         return
 
+# List / Sort / Search Dataframe + Dataframe Statistics
 def explore_data():
-    global df
+
+    global df   # Apparently Necessary to allow other functs to access Dataframe
+
     while True:
         print("\
                         -----------------Exploring Data--------------\n \
@@ -111,41 +130,60 @@ def explore_data():
                         [4]Search Element in Column                  \n \
                                     [c]Clear Console                 \n \
                                     [d]Return to Main Menu           \n")
+        
+        # Get User's Selection
         choice = input("Enter an option: ")
+
+        # Switch for User's Selection
         if choice == "d":
-            break
+            break                   # Return to Main Menu
+
         elif choice == "c":
-            clear_console()
+            clear_console()         # Clear Console Display
+
         elif choice == "1":
+
+            # If Dataframe is Empty, Return to Main Menu
             if df.empty:
                 print("DataFrame is empty")
                 return
             
+            # Display DataFrame Column Headers in a Rich Table 
             table = Table(title="This is Table")
             for column_headers in df.columns:
                 table.add_column(column_headers, justify="right", style="cyan", overflow="fold")
             console.print(table)
+
         elif choice == "2":
+
+             # If Dataframe is Empty, Return to Local Menu
             if df.empty:
                 print("DataFrame is empty")
                 continue
             
+            # Get User's Selection
             col_name = input("Input Column Name to Drop from the list: ")
+
+            # Drop Column if it exists in Dataframe
             if col_name not in df.columns:
                 print("Error: Column name does not exist in dataframe")
                 continue
-            df = df.drop(col_name, axis=1)
 
+            df = df.drop(col_name, axis=1)
             if col_name not in df.columns:
                 print(f'Column [ "{col_name}" ] dropped from dataframe')
+
             else:
                 print(f'Error dropping column [ "{col_name}" ] from dataframe')
+
         elif choice == "3":
             if df.empty:
                 print("DataFrame is empty")
                 continue
 
+            # Get User's Selection
             col_name = input(f"[{current_time()}] Input Column Name to Describe: ")
+
             if col_name not in df.columns:
                 print("Error: Column name does not exist in dataframe")
                 continue
@@ -188,16 +226,21 @@ def explore_data():
 
         else:
             print("Invalid choice.")
-    
+
+# Display Dataframe In Rich Table
 def print_data():
-################# count unique values in col #####################
+
     if df.empty:
         print("DataFrame is empty")
         return
     
+    # Get User's Selection
     quantity = input("How many rows would you like to see, please input a number from 1 to " +  str(len(df)) + " \n")
+
+    # Pass DataFrame & Return Select Range of Rows
     first_five = first_5(df, int(quantity))
 
+    # Display DataFrame in Rich Table
     table = Table(title="This is Table")
 
     for column_headers in df.columns:
@@ -210,40 +253,6 @@ def print_data():
 
     console.print(table) 
 
-"""
-    elif user_input == '5':
-        ################# count unique values in col #####################
-        if df.empty:
-            print("DataFrame is empty")
-            continue
-
-        print(df.columns.to_list())
-        col_name = input("Column name:  ")
-        
-        if col_name not in df.columns:
-            print("Error: Column name does not exist in dataframe")
-            continue
-        
-        col_count = count_all_values(df, col_name)
-        print(col_count)
-
-    elif user_input == '6':
-        ################# count unique values in col #####################
-        if df.empty:
-            print("DataFrame is empty")
-            continue
-
-        print(df.columns.to_list())
-        col_name = input("Column name:  ")
-        
-        if col_name not in df.columns:
-            print("Error: Column name does not exist in dataframe")
-            continue
-        
-        unique_col_count = count_unique_values(df, col_name)
-        print(len(unique_col_count))
-
-        """
 if __name__ == "__main__":
     while True:
         # Display Main Menu
